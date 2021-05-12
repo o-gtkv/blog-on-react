81e22cf9fb98cfd9ea5fd434472592f12cc1e0a1
Oleg <oleg_g87@outlook.com>
2021-05-12 11:37:42 +0300

    added created CenteredItemContainer

---
+++ src/App/Main/Components/CenteredItemsContainer/CenteredItemsContainer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const CenteredItemsContainer = ({backgroundImage, height, children}) => {
+    const style = {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        height: height,
+        backgroundImage: `url(${backgroundImage})`
+        backgroundAttachment: 'fixed    '
+    }
+
+    return (
+        <div style={style}>
+            {children}
+        </div>
+    )
+}
+
+export default CenteredItemsContainer
